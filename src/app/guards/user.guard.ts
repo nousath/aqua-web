a70c9747bc57ef0e762db810dfc5485407063fe8
ravikant <ravikant.verma@mindfulsas.com>
2017-09-29 12:06:40 +0530

    Text Align Fixes and Text Fixes

--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -3,6 +3,9 @@ import { Router, Route } from '@angular/router';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 import { LocalStorageService } from '../services/local-storage.service';
+import { User } from '../models/user';
+import { Employee } from '../models/employee';
+declare var ga: any;
 
 @Injectable()
 export class UserGuard implements CanActivate {
@@ -11,12 +14,15 @@ export class UserGuard implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-    // let user: User = this.store.getObject('user') as User;
+    let user: Employee = this.store.getObject('user') as Employee;
     let amstoken: string = this.store.getItem('ams_token');
     let orgCode: string = this.store.getItem('orgCode');
     if (!amstoken || !orgCode) {
       this.router.navigate(['/login']);
       return false;
+    }
+    if (user) {
+      ga('set', '&uid', `${user.name} ${user.email}`)
     }
     return true;
   }
