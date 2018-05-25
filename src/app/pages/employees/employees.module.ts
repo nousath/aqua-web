063b68c3b1b956373753e65f122af5f4abba1084
Saurabh.pandey <saurabh.pandey@mindfulsas.com>
2018-05-25 15:38:22 +0530

    build issue fixed

--- src/app/pages/employees/employees.module.ts
+++ src/app/pages/employees/employees.module.ts
@@ -6,7 +6,7 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmpEditComponent } from './emp-edit/emp-edit.component';
 import { DesignationsComponent } from './designations/designations.component';
 import { ConfirmDialogComponent } from '../../dialogs/confirm-dialog/confirm-dialog.component';
-import { ResetPasswordDialogComponent } from '../../dialogs/reset-password-dialog/reset-password-dialog.component';
+// import { ResetPasswordDialogComponent } from '../../dialogs/reset-password-dialog/reset-password-dialog.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
@@ -20,11 +20,11 @@ import { SharedModule } from '../../shared/shared.module';
     EmpEditComponent,
     DesignationsComponent,
     ConfirmDialogComponent,
-    ResetPasswordDialogComponent
+    // ResetPasswordDialogComponent
   ],
   entryComponents: [
     ConfirmDialogComponent,
-    ResetPasswordDialogComponent
+    // ResetPasswordDialogComponent
   ],
   exports: [
     EmployeesComponent,
@@ -32,7 +32,7 @@ import { SharedModule } from '../../shared/shared.module';
     EmpEditComponent,
     DesignationsComponent,
     ConfirmDialogComponent,
-    ResetPasswordDialogComponent
+    // ResetPasswordDialogComponent
   ]
 })
 export class EmployeesModule { }
