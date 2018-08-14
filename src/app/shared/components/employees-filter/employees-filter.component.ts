45eaf6a950da91fe821a1a9c4bba7e515534724e
Saurabh.pandey <saurabh.pandey@mindfulsas.com>
2018-08-14 19:05:16 +0530

    Filter multiselect and Shift roster bug fixed

--- src/app/shared/components/employees-filter/employees-filter.component.ts
+++ src/app/shared/components/employees-filter/employees-filter.component.ts
@@ -6,6 +6,8 @@ import { TagType } from '../../../models/tag';
 import { AmsTagService } from '../../../services/ams/ams-tag.service';
 import { Department } from '../../../models/department';
 import { EmsDepartmentService } from '../../../services/ems/ems-department.service';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
+import { Item } from 'angular2-multiselect-dropdown/menu-item';
 
 export interface SelectedTag {
   tagId: string;
@@ -79,7 +81,26 @@ export class EmployeesFilterComponent implements OnInit {
 
   tags: Tags = new Tags();
 
-  selectedValue: boolean
+  departmentList = [];
+  designationList = [];
+  designations = [];
+  departments = [];
+  usertypes = [];
+  contractors
+ 
+  userTypeList = [];
+  contractorList = [];
+  selectedDesignation = [];
+  selectedDepartment = [];
+  selectedUsertype = [];
+  selectedContractor = [];
+  selectedStatus = [];
+  statusList = [];
+  status = [];
+  dropdownSettings = {};
+  selectedValue: boolean;
+
+
   constructor(
     public validatorService: ValidatorService,
     amsShiftService: AmsShiftService,
@@ -96,10 +117,111 @@ export class EmployeesFilterComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.tagTypes.fetch().then(result => {
+      this.getTags(result.items);
+    });
     this.shiftTypes.fetch().then();
-    this.tagTypes.fetch().then();
-
     this.hidden = this.hidden || {};
+    this.statusList = [{
+      id:1 , itemName: 'All'
+    },{
+      id:2 , itemName: 'Present'
+    },{
+      id:3 , itemName: 'Absent'
+    },{
+      id:4 , itemName: 'MissedSwipe'
+    },{
+      id:5 , itemName: 'HalfDay'
+    },{
+      id:6 , itemName: 'OnLeave'
+    },{
+      id:7 , itemName: 'LateComing'
+    },{
+      id:8 , itemName: 'EarlyGoing'
+    },
+  ]
+  }
+
+  getTags(tags: any) {
+    tags.forEach(item => {
+      switch (item.name.toLowerCase()) {
+        case 'department':
+          this.departments = item.tags;
+          break;
+        case 'designation':
+          this.designations = item.tags;
+          break;
+        case 'usertype':
+          this.usertypes = item.tags;
+          break;
+        case 'contractor':
+          this.contractors = item.tags;
+          break;
+      }
+    });
+
+    this.designations.forEach(item =>{
+    let  obj = {
+        id: item.id,
+        itemName: item.name,
+      };
+      this.designationList.push(obj);
+    })
+    console.log(this.designationList);
+
+    this.departments.forEach(item =>{
+    let  obj = {
+        id: item.id,
+        itemName: item.name,
+      };
+      this.departmentList.push(obj);
+    })
+    console.log(this.departmentList);
+
+    this.contractors.forEach(item =>{
+    let  obj = {
+        id: item.id,
+        itemName: item.name,
+      };
+      this.contractorList.push(obj);
+    })
+    console.log(this.contractorList);
+
+    this.usertypes.forEach(item =>{
+    let  obj = {
+        id: item.id,
+        itemName: item.name,
+      };
+      this.userTypeList.push(obj);
+    })
+    console.log(this.userTypeList);
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      text: 'Select an option',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      classes: "myclass",
+      displayAllSelectedText: true,
+      maxHeight:200,
+      badgeShowLimit:1
+    };
+
+  }
+  onItemSelect(item: any) {
+    console.log(item.id);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item.id);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
   }
 
   reset() {
@@ -108,6 +230,11 @@ export class EmployeesFilterComponent implements OnInit {
     if (tagElements) {
       tagElements.forEach(item => item.value = '');
     }
+    this.selectedDesignation = [];
+    this.selectedDepartment = [];
+    this.selectedUsertype = [];
+    this.selectedContractor = [];
+    this.selectedStatus = [];
     this.tags.selected = []
     this.selectedShiftType = null;
     this.selectedAttendanceStatus = null;
@@ -119,26 +246,29 @@ export class EmployeesFilterComponent implements OnInit {
 
   apply() {
     const tagIds: string[] = [];
-    this.tags.selected.forEach((tag: SelectedTag) => {
-      if (tagIds.length) {
-        const index = tagIds.indexOf(tag.tagId)
-        if (index >= 0) {
-          tagIds.splice(index, 1)
-          tagIds.push(tag.tagId)
-        }
-        else {
-          tagIds.push(tag.tagId)
-        }
-      }
-      else {
-        tagIds.push(tag.tagId)
-      }
+    this.selectedDesignation.forEach(item=>{
+      tagIds.push(item.id);
+    })
+    this.selectedDepartment.forEach(item=>{
+      tagIds.push(item.id);
+    })
+    this.selectedUsertype.forEach(item=>{
+      tagIds.push(item.id);
+    })
+    this.selectedContractor.forEach(item=>{
+      tagIds.push(item.id);
+    })
+    console.log(tagIds)
+
+    const status: string[] = [];
+    this.selectedStatus.forEach(item =>{
+      status.push(item.itemName);
     })
 
     const values = {
       tagIds: tagIds,
       shiftType: this.selectedShiftType,
-      attendanceStatus: this.selectedAttendanceStatus,
+      attendanceStatus: status,
       employeeName: this.selectedEmployeeName,
       employeeCode: this.selectedEmployeeCode
     }
